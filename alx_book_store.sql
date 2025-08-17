b51a01d37c420540a0896526ef3fb641cceaeb4e
Lidet <lidetadmassu217@gmail.com>
2025-08-17 22:44:00 +0300

    Initial commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,7 +25,7 @@ CREATE TABLE Customers(
 CREATE Orders(
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE,
 
 )
@@ -33,7 +33,7 @@ CREATE TABLE Order_Details(
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 )
