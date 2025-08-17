1c1a10c1ab6e6a0e0769886c496dd76cfa093b5f
Lidet <lidetadmassu217@gmail.com>
2025-08-17 23:49:51 +0300

    Initial commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,39 +1,22 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store
-USE alx_book_store
-
-CREATE TABLE Authors(
-    author_id PRIMARY KEY INT AUTO_INCREMENT,
-    author_name VARCHAR(215) NOT NULL,
-)
-
-CREATE TABLE Books(
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(130) NOT NULL,
-    price DOUBLE,
-    publication_date DATE,
-    author_id INT,
-    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
-)
-
-CREATE TABLE Customers(
-    customer_id PRIMARY KEY INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE,
-    address TEXT,
-) 
+    address TEXT
+);
 
-CREATE Orders(
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
 
-)
-CREATE TABLE Order_Details(
-    order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Order_Details(
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    quantity DOUBLE
-)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
