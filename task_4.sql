21c84372bc56b225db56cd9cdf594a50f2731980
Lidet <lidetadmassu217@gmail.com>
2025-08-18 00:26:01 +0300

    Initial commit

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+USE alx_book_store;
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM
+    information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND
+    TABlE_NAME = 'books'
+
